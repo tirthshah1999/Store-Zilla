707e227565c3885a37481624825bbc744479efb3
Tirth <tirthcoder@gmail.com>
2022-02-25 10:35:13 +0530

    product sort functionality added

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -2,8 +2,33 @@ import React from 'react'
 import { useFilterContext } from '../context/filter_context'
 import { BsFillGridFill, BsList } from 'react-icons/bs'
 import styled from 'styled-components'
+
 const Sort = () => {
-  return <h4>sort </h4>
+  const {filteredProducts: products, gridView, setGridView, setListView, sort, updateSort} = useFilterContext();
+
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button onClick={setGridView} className={`${gridView ? 'active' : null}`}>
+          <BsFillGridFill />
+        </button>
+        <button onClick={setListView} className={`${!gridView ? 'active' : null}`}>
+          <BsList />
+        </button>
+      </div>
+      <p>{products.length} products found</p>
+      <hr />
+      <form>
+        <label htmlFor="sort">sort by</label>
+        <select name="sort" id="sort" className="sort-input" value={sort} onChange={updateSort}>
+          <option value='price-lowest'>price (lowest)</option>
+          <option value='price-highest'>price (highest)</option>
+          <option value='name-a'>name (a - z)</option>
+          <option value='name-z'>name (z - a)</option>
+        </select>
+      </form>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -13,9 +13,41 @@ const filter_reducer = (state, action) => {
   switch(action.type) {
     case LOAD_PRODUCTS:
       return {...state, allProducts: [...action.payload], filteredProducts: [...action.payload]}
+    
+    case SET_GRIDVIEW:
+      return {...state, gridView: true}
+    
+    case SET_LISTVIEW:
+      return {...state, gridView: false}
+
+    case UPDATE_SORT:
+      return {...state, sort: action.payload}
+    
+    case SORT_PRODUCTS:
+      const {sort, filteredProducts} = state;
+      let tempProducts = [];
+      
+      if(sort === 'price-lowest'){
+        tempProducts = filteredProducts.sort((a,b) => a.price - b.price);
+      }
+
+      if(sort === 'price-highest'){
+        tempProducts = filteredProducts.sort((a,b) => b.price - a.price)
+      }
+
+      if(sort === 'name-a'){
+        tempProducts = filteredProducts.sort((a,b) => a.name.localeCompare(b.name))
+      }
+
+      if(sort === 'name-z'){
+        tempProducts = filteredProducts.sort((a,b) => b.name.localeCompare(a.name))
+      }
+
+      return {...state, filteredProducts: tempProducts}
+      
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`)
   }
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
 }
 
 export default filter_reducer
